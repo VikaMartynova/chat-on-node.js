d65c42195db19b219dc7cea9be688f1dc56bc462
viktoria <vikam1210@gmail.com>
2018-07-05 11:23:24 +0300

    create chat

--- app/calc.js
+++
@@ -1,8 +0,0 @@
-
-function sum(arr) {
-    return arr.reduce(function (s, a) {
-        return s + a;
-    }, 0)
-}
-
-module.exports.sum = sum;
---
+++ server.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var fs = require('fs');
+var chat = require('./chat');
+
+http.createServer(function (req, res) {
+
+    switch (req.url) {
+        case '/':
+            sendFile('index.html', res);
+            break;
+        case '/subscribe':
+            chat.subscribe(req, res);
+            break;
+        case '/publish':
+            chat.publish('hello');
+            break;
+        default:
+            res.statusCode = 404;
+            res.end("Not Found");
+    }
+
+}).listen(3000);
+
+function sendFile(fileName, res) {
+    var fileStream = fs.createReadStream(fileName);
+    fileStream
+        .on('error', function () {
+            res.statusCode = 500;
+            res.end("Server error");
+        });
+}
